a5d26dc381d5409a258a7b8adc464bfc4051dd6a
Yurley Estefany Rueda Romero <misiontic.tutor31@uis.edu.co>
2022-09-22 20:25:02 -0500

    Requested corrections
    Se realizaron las siguientes correcciones:
    Cambio a colores establecidos en figma.
    Se creó el script por aparte y cambio a const de la variable del script.
    Cambio de rutas de iconos e imagen.
    Cards de videos aparte de los ya creados.
    Cambio de texto a h2.

---
+++ src/script.js
@@ -0,0 +1,9 @@
+const icon =document.getElementById("icon");
+icon.onclick=function(){
+    document.body.classList.toggle("theme-dark");
+    if(document.body.classList.contains("theme-dark")){
+        icon.src="/icons/sun-icon.svg";
+    }else{
+        icon.src="/icons/moon-icon.svg";
+    }
+}
