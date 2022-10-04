dde6e46d24195d0f5b57bf0176e66475e1f84015
SilviaPabon <silvia.pabon.2020@upb.edu.co>
2022-10-04 05:52:35 -0500

    draft of response with setInterval

--- src/script.js
+++ src/script.js
@@ -16,36 +16,87 @@ icon.onclick = function () {
 // requesting videos
 formulario.addEventListener('submit', async (e) => {
 	e.preventDefault();
-	const response = await fetch(
-		''
-	);
-	if (response.ok) {
-		const data = await response.json();
-		const videos = data.response;
-		const template = document.querySelector('template');
-		// todo: definir dónde se inserta
-		const section = document.getElementById('articles-container');
+	const responseKey = await fetch('http://localhost:9090/search', {
+		method: 'POST',
+		headers: { 'Content-Type': 'application/json' },
+		body: JSON.stringify(Object.fromEntries(new FormData(formulario))),
+	});
+	const dataKey = await responseKey.json();
 
-		videos.forEach((element) => {
-
-			const tags = element.tags.split(', ', 3);
-			const templateClone = template.content.cloneNode(true);
-			const templateCloneTitle = templateClone.getElementById('title');
-
-			templateCloneTitle.textContent = element.title;
-			templateCloneTitle.setAttribute('href', element.url);
-			templateClone.getElementById('url').setAttribute('href', element.url);
-			templateClone.querySelector('img').setAttribute('src', element.thumbnail);
-
-			const assignTags = templateClone.getElementById('tags').querySelectorAll('p');
-			for (let ii = 0; ii < 3; ii++) {
-				assignTags[ii].textContent = tags[ii];
-			}
-
-			section.appendChild(templateClone);
+	let intervalID = setInterval(async () => {
+		const res = await fetch('http://localhost:9090/result/obtain', {
+			method: 'POST',
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify({ key: dataKey.uuid }),
 		});
-	} else {
-		console.log('Nothing');
-	}
-});
+		let data = await res;
 
+		if (res.status == 200) {
+			clearInterval(intervalID);
+			data = await data.json();
+			console.log('object', data.response);
+			let section = '';
+
+			const videos = data.response;
+			const template = document.getElementById('template-2');
+			section = document.getElementById('articles-container');
+			const cards = section.querySelectorAll('.video-placeholder');
+			cards.forEach((card) => card.remove());
+			videos.forEach((element) => {
+				const tags = element.tags.split(', ', 3);
+				const templateClone = template.content.cloneNode(true);
+				const templateCloneTitle = templateClone.getElementById('title');
+
+				templateCloneTitle.textContent = element.title;
+				templateCloneTitle.setAttribute('href', element.url);
+				templateClone.getElementById('url').setAttribute('href', element.url);
+				templateClone.querySelector('img').setAttribute('src', element.thumbnail);
+
+				const assignTags = templateClone.getElementById('tags').querySelectorAll('p');
+				for (let ii = 0; ii < 3; ii++) {
+					assignTags[ii].textContent = tags[ii];
+				}
+
+				section.appendChild(templateClone);
+			});
+		} else {
+			loading();
+		}
+	}, 2000);
+
+});
+loading();
+
+function loading() {
+	const section = document.getElementById('articles-container');
+	const templateOne = document.querySelector('template');
+	for (let ii = 0; ii < 6; ii++) {
+		const templateCloneOne = templateOne.content.cloneNode(true);
+		section.appendChild(templateCloneOne);
+	}
+}
+
+/* function startTimer(data, dataKey) {
+	let data2;
+	const video = setInterval(async function () {
+		await fetch('http://localhost:9090/result/obtain', {
+			method: 'POST',
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify({ key: dataKey.uuid }),
+		})
+			.then(function (response) {
+				data = response;
+				data2 = response.json();
+				console.log(data2, 'sss');
+				if (response.status == 200) {
+					clearInterval(video);
+				}
+				return data2;
+			})
+			.then(function (data) {
+				console.log('function', data);
+				return data;
+			});
+	}, 5000);
+	return data;
+} */
